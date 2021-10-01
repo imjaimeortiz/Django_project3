48e5e93b198088861fdc9facbd6e0a18678d87c9
imjaimeortiz <jaimeortizlaflota@gmail.com>
2021-10-01 16:54:45 +0200

    blogs dynamic files

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -3,7 +3,16 @@ from django.db.models.fields import CharField, DateField, TextField
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=20, default='')
-    pub_date = models.DateField(null=True)
-    body = models.TextField(default='')
-    image = models.ImageField(upload_to='images/', null=True)
+    title = models.CharField(max_length=20)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+    
+    def print_date(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
