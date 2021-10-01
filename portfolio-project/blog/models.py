9f244f00172a747112632b6ab74033fa6580a48b
imjaimeortiz <jaimeortizlaflota@gmail.com>
2021-10-01 10:51:32 +0200

    Using bootstrap

--- portfolio-project/blog/models.py
+++ portfolio-project/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.db.models.fields import CharField
+from django.db.models.fields import CharField, DateField, TextField
 
 # Create your models here.
 class Blog(models.Model):
-    name = CharField(max_length=500)
+    title = models.CharField(max_length=20, default='')
+    pub_date = models.DateField(null=True)
+    body = models.TextField(default='')
+    image = models.ImageField(upload_to='images/', null=True)
